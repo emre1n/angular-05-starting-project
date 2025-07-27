88ff0ce38a72d0abecfe6c4d6edb6b1684c6b6cd
emre1n <emre1n@protonmail.com>
2025-07-27 21:33:50 +0300

    inject hostElementRef instead of targeting the host element with event target

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -9,6 +9,7 @@ import { Directive, input } from '@angular/core';
 })
 export class SafeLinkDirective {
   queryParam = input('myapp', { alias: 'appSafeLink' });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
   constructor() {
     console.log('SafeLinkDirective is active!');
@@ -18,8 +19,14 @@ export class SafeLinkDirective {
     const wantsToLeave = window.confirm('Do you want to leave the app?');
 
     if (wantsToLeave) {
-      const address = (event.target as HTMLAnchorElement).href;
-      (event.target as HTMLAnchorElement).href =
+      // @NOTE: This is the old way to get the href of the anchor element
+      //   const address = (event.target as HTMLAnchorElement).href;
+      //   (event.target as HTMLAnchorElement).href =
+      //     address + '?from=' + this.queryParam();
+
+      // @NOTE: This is the new way to get the href of the anchor element by using the ElementRef injected
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href =
         address + '?from=' + this.queryParam();
       return;
     }
